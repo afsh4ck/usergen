68870dba9c54da5a4363ee5204ef42b8f0b01403
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2023-07-26 09:30:46 +0200

    Update usergen.py

--- usergen.py
+++ usergen.py
@@ -34,13 +34,20 @@ divider = """------------------------------------------------------------
 def generar_nombres_usuario(nombre, apellidos):
     nombres_usuario = set()
     nombre = nombre.lower().replace(" ", "")  # Eliminar espacios entre palabras del nombre
-    apellidos = apellidos.lower().replace(" ", "")  # Eliminar espacios entre palabras del apellido
+
+    # Verificar si el apellido no está vacío antes de obtener la primera letra
+    if apellidos:
+        apellidos = apellidos.lower().replace(" ", "")  # Eliminar espacios entre palabras del apellido
+        primera_letra_apellidos = apellidos[0]
+    else:
+        apellidos = ""
+        primera_letra_apellidos = ""
 
     # Obtener el año actual
     año_actual = datetime.datetime.now().year
 
-    # Combinar nombre, apellidos y el año actual y variantes de hasta 50 años menos sin separadores
-    for num in range(51):
+    # Combinar nombre, apellidos y el año actual y variantes de hasta 70 años menos sin separadores
+    for num in range(71):
         año = año_actual - num
         nombres_usuario.add(nombre + apellidos)
         nombres_usuario.add(nombre + str(año) + apellidos)
@@ -51,22 +58,21 @@ def generar_nombres_usuario(nombre, apellidos):
 
     # Agregar nuevas variantes reemplazando el nombre por la primera letra del nombre y el apellido por la primera letra del apellido
     primera_letra_nombre = nombre[0]
-    primera_letra_apellidos = apellidos[0]
     nombres_usuario.add(primera_letra_nombre + apellidos)
     nombres_usuario.add(apellidos + primera_letra_nombre)
-    nombres_usuario.add(primera_letra_nombre + apellidos + str(año))
-    nombres_usuario.add(apellidos + primera_letra_nombre + str(año))
-    nombres_usuario.add(primera_letra_nombre + str(año) + apellidos)
+    nombres_usuario.add(primera_letra_nombre + apellidos + str(año_actual))
+    nombres_usuario.add(apellidos + primera_letra_nombre + str(año_actual))
+    nombres_usuario.add(primera_letra_nombre + str(año_actual) + apellidos)
     nombres_usuario.add(primera_letra_apellidos + nombre)
     nombres_usuario.add(nombre + primera_letra_apellidos)
-    nombres_usuario.add(primera_letra_apellidos + nombre + str(año))
-    nombres_usuario.add(nombre + primera_letra_apellidos + str(año))
-    nombres_usuario.add(primera_letra_apellidos + str(año) + nombre)
+    nombres_usuario.add(primera_letra_apellidos + nombre + str(año_actual))
+    nombres_usuario.add(nombre + primera_letra_apellidos + str(año_actual))
+    nombres_usuario.add(primera_letra_apellidos + str(año_actual) + nombre)
 
-    # Combinar nombre y apellidos con puntos, guiones y guiones bajos, y el año actual y variantes de hasta 50 años menos
+    # Combinar nombre y apellidos con puntos, guiones y guiones bajos, y el año actual y variantes de hasta 70 años menos
     separadores = ['.', '-', '_']
     for separador in separadores:
-        for num in range(51):
+        for num in range(71):
             año = año_actual - num
             nombres_usuario.add(nombre + separador + apellidos)
             nombres_usuario.add(nombre + separador + str(año) + separador + apellidos)
@@ -88,6 +94,23 @@ def generar_nombres_usuario(nombre, apellidos):
             nombres_usuario.add(primera_letra_apellidos + separador + str(año) + separador + nombre)
             nombres_usuario.add(nombre + separador + str(año) + separador + primera_letra_apellidos)
 
+            # Agregar nuevas variantes cuando el apellido no está presente
+            nombres_usuario.add(nombre)
+            nombres_usuario.add(nombre + str(año))
+            nombres_usuario.add(nombre + separador + str(año))
+            nombres_usuario.add(str(año) + nombre)
+            nombres_usuario.add(str(año) + separador + nombre)
+            nombres_usuario.add(primera_letra_nombre + str(año))
+            nombres_usuario.add(primera_letra_nombre + separador + str(año))
+            nombres_usuario.add(str(año) + primera_letra_nombre)
+            nombres_usuario.add(str(año) + separador + primera_letra_nombre)
+
+    # Filtrar nombres de usuario vacíos (cuando el apellido está vacío) y los que empiezan o terminan con un separador
+    nombres_usuario = [nombre_usuario for nombre_usuario in nombres_usuario if nombre_usuario and not nombre_usuario.startswith(tuple(separadores)) and not nombre_usuario.endswith(tuple(separadores))]
+
+    # Ordenar por año, primero los nombres con el año actual y luego los años anteriores
+    nombres_usuario = sorted(nombres_usuario, key=lambda x: (str(año_actual) in x, x.find(str(año_actual)), x), reverse=True)
+
     return nombres_usuario
 
 def exportar_nombres_usuario(nombres_usuario, archivo):
@@ -101,7 +124,7 @@ while True:
 
     # Pedir al usuario que ingrese su nombre y apellidos
     nombre = input(VERDE + "[+] Introduce un nombre: " + RESET)
-    apellidos = input(VERDE + "[+] Introduce apellidos o palabras clave (separados por espacios): " + RESET)
+    apellidos = input(VERDE + "[+] Introduce un apellido o palabra clave: " + RESET)
 
     # Generar los nombres de usuario
     nombres_usuario = generar_nombres_usuario(nombre, apellidos)
